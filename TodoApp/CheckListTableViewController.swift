69959e5b2c976066ca691fa9ccbba7969b1876cb
donioscode <doniyornormuxammedov@gmail.com>
2024-02-17 13:24:31 +0500

    checkmark added

--- TodoApp/CheckListTableViewController.swift
+++ TodoApp/CheckListTableViewController.swift
@@ -13,11 +13,45 @@ class CheckListTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
+     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "checklistitem", for: indexPath)
+        
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "This is first row"
+        }else if indexPath.row == 2 {
+            label.text = "This is second row"
+        }else if indexPath.row == 3 {
+            label.text = "This is third row"
+        }else if indexPath.row == 4 {
+            label.text = "This is four row"
+        }else{
+            label.text = "This is last row"
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            }else{
+                cell.accessoryType = .checkmark
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
