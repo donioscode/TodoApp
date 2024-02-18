21e21b284bc3bbd37637690faebcc64a81286180
donioscode <doniyornormuxammedov@gmail.com>
2024-02-18 16:45:34 +0500

    doneBarButton updated

--- TodoApp/AddItemTableViewController.swift
+++ TodoApp/AddItemTableViewController.swift
@@ -9,16 +9,44 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
 
+    @IBOutlet weak var addItemTF: UITextField!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
+        doneBarButton.isEnabled = false
+        addItemTF.delegate = self
         navigationController?.navigationItem.largeTitleDisplayMode = .never
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addItemTF.becomeFirstResponder()
+    }
+    
     @IBAction func done(){
         navigationController?.popViewController(animated: true)
+    
     }
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        
+        let stringRange = Range(range,in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        }else{
+            doneBarButton.isEnabled = true
+        }
+        
+        return true
+    }
+    
 }
