85e2f09ab51798d83ef0ffc86c434aaa4d0c1ca0
donioscode <doniyornormuxammedov@gmail.com>
2024-02-27 10:14:15 +0500

    user notification first work

--- TodoApp/App/AppDelegate.swift
+++ TodoApp/App/AppDelegate.swift
@@ -16,13 +16,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert,.sound]) { granted , error in
-            if granted {
-                print("We have permission")
-            } else {
-                print("Permission denied")
-            }
-        }
+        center.delegate = self
+        
+//        center.requestAuthorization(options: [.alert,.sound]) { granted , error in
+//            if granted {
+//                print("We have permission")
+//            } else {
+//                print("Permission denied")
+//            }
+//        }
+//       
+//        
+//        let content = UNMutableNotificationContent()
+//        content.title = "Hello"
+//        content.body = "I am local notification"
+//        content.sound = UNNotificationSound.default
+//        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+//        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
+//        center.add(request)
+        
         return true
     }
 
@@ -39,7 +52,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Recived local notification")
+    }
+}
