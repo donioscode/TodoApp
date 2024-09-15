56292e426c96a8c81c1c5d66a2f2b74a3fc06473
donioscode <doniyornormuxammedov@gmail.com>
2024-09-15 15:50:07 +0500

    delete Auth

---
+++ TodoApp/Auth/AuthViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by doniyor normuxammedov on 28/02/24.
+//
+
+import UIKit
+
+class AuthViewController: UITableViewController {
+
+    @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
